df4bc98b3f24237b10b9c8d78a395b4e4c8f1cec
Rushabh Doshi <rush3doshi@gmail.com>
2026-08-09 18:36:53 -0400

    tests added for timer

--- src/timer.rs
+++ src/timer.rs
@@ -1,23 +1,54 @@
 pub struct Timer {
-    time: u8,
+    value: u8,
 }
 
 impl Timer {
     pub fn new() -> Self {
-        Timer { time: 0 }
+        Timer { value: 0 }
     }
 
-    pub fn set(&mut self, time: u8) {
-        self.time = time;
+    pub fn set(&mut self, value: u8) {
+        self.value = value;
     }
 
     pub fn get(&self) -> u8 {
-        self.time        
+        self.value        
     }
 
     pub fn tick(&mut self) {
-        if self.time > 0 {
-            self.time -= 1;
+        if self.value > 0 {
+            self.value -= 1;
         }
+    }
+}
+
+#[cfg(test)]
+mod time_tests {
+    use crate::timer::Timer;
+
+    #[test]
+    fn new_timer_starts_at_zero() {
+        let timer = Timer::new();
+
+        assert_eq!(0, timer.get());
+    }
+
+    #[test]
+    fn tick_does_not_go_below_zero() {
+        let mut timer = Timer::new();
+
+        timer.tick();
+
+        assert_eq!(0, timer.get());
+    }
+    
+    #[test]
+    fn tick_decrements_time() {
+        let mut timer = Timer::new();
+        
+        timer.set(60);
+        timer.tick();
+
+        assert_eq!(59, timer.get());
     }
 }
