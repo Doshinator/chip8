e06c0e37bdb14035074bcb363d0b8d5fcc72a171
Rushabh Doshi <rush3doshi@gmail.com>
2026-08-16 17:51:21 -0400

    main driver added

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,16 @@ use chip8::chip8::Chip8;
 
 fn main() {
     println!("CHIP-8 emulator");
-    let _emulator = Chip8::new();
+    let rom = std::fs::read("roms/test.ch8")
+        .expect("failed to read ROM");
+
+    let mut emulator = Chip8::new();
+
+    emulator
+        .load_rom(&rom)
+        .expect("failed to open ROM");
+
+    loop {
+        emulator.tick().expect("CHIP-8 execution failed");
+    }
 }
